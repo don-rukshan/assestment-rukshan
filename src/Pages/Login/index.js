64162874a32519799d36bcf472e8f9a142ae9381
Don Rukshan <md.rukshan@gmail.com>
2021-04-13 09:03:40 +0530

    login page styling issue fixed

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -3,6 +3,8 @@ import { signInUser } from "../../redux/api/profile.api";
 import { useHistory } from "react-router-dom";
 import bg from "../../assets/bg.jpg";
 
+import "./styles.css";
+
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -14,7 +16,7 @@ const LoginPage = () => {
     console.log(username, password);
     signInUser(username, password)
       .then((res) => {
-        console.log("Adoooooooooooo", res);
+        console.log("Response", res);
         setInvalidUser(false);
         localStorage.setItem("user-info", JSON.stringify(username));
         history.push("/dashboard");
@@ -26,129 +28,41 @@ const LoginPage = () => {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flex: 1,
-        flexDirection: "row",
-        height: "100vh",
-        overflow: "hidden",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flex: 2,
-          maxHeight: "100%",
-          overflow: "hidden",
-        }}
-      >
+    <div className="outer__container">
+      <div className="left__side">
         <img style={{ width: "100%" }} src={bg} alt="bg" />
       </div>
 
-      <div
-        style={{
-          display: "flex",
-          flex: 2,
-          flexDirection: "column",
-          height: "100%",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          background: "whitesmoke",
-        }}
-      >
-        <div
-          style={{
-            color: "rgba(0,0,0,0.4)",
-            fontSize: "27px",
-            fontWeight: "400",
-            marginTop: "28%",
-            marginBottom: "21px",
-          }}
-        >
-          Login to continue
-        </div>
+      <div className="right__side">
+        <div className="right__text">Login to continue</div>
 
-        <div>
+        <div className="form_input">
           <input
             type="text"
             placeholder="Username"
             onChange={(e) => setUsername(e.target.value)}
             className="form-control"
-            style={{
-              height: "45px",
-              border: "1px solid rgba(0,0,0,0.1)",
-              borderRadius: "6px",
-              padding: "2px",
-              paddingLeft: "6px",
-              width: "300px",
-            }}
           />
+
           <div style={{ marginTop: 10 }} />
           <input
             type="password"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
             className="form-control"
-            style={{
-              height: "45px",
-              border: "1px solid rgba(0,0,0,0.1)",
-              borderRadius: "6px",
-              padding: "2px",
-              paddingLeft: "6px",
-              width: "300px",
-            }}
           />
+
           <div style={{ marginTop: 20 }} />
-          <button
-            onClick={() => handleLogin()}
-            className="btn btn-primary"
-            style={{
-              height: "35px",
-              border: "1px solid rgba(0,0,0,0.1)",
-              borderRadius: "6px",
-              padding: "2px",
-              paddingLeft: "6px",
-              width: "308px",
-              textAlign: "center",
-              background: "#ff124a",
-              color: "white",
-            }}
-          >
+          <button onClick={() => handleLogin()} className="btn btn-primary">
             Login
           </button>
         </div>
       </div>
 
       {!invalidUser || (
-        <div
-          style={{
-            height: "25px",
-            padding: "15px",
-            position: "absolute",
-            right: 14,
-            top: 14,
-            background: "pink",
-            textAlign: "center",
-            borderRadius: "14px",
-          }}
-        >
-          Invalid User
-          <button
-            style={{
-              background: "none",
-              border: "1px solid rgba(0,0,0,0.15)",
-              height: "25px",
-              width: "25px",
-              textAlign: "center",
-              borderRadius: "12px",
-              marginLeft: "8px",
-              cursor: "pointer",
-            }}
-            onClick={() => setInvalidUser(false)}
-          >
-            X
-          </button>
+        <div className="error__msg">
+          Invalid Credentials
+          <button onClick={() => setInvalidUser(false)}>X</button>
         </div>
       )}
     </div>
